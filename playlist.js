9a453f5f15d39e7ecd0a313a156715b0c682e1f3
Ozzy Cohen Abramson <odentionofficial@gmail.com>
2021-06-21 13:28:12 +0300

    best song everrrr!

--- playlist.js
+++ playlist.js
@@ -1,5 +1,6 @@
 const playlist = [
-    { singer: "Maluma", song: "Despacito" },
-    { singer: "Noa Kirel", song: "Diamonds" },
-    { singer: "ACDC", song: "Thunderstruck" }
+  { singer: "Maluma", song: "Despacito" },
+  { singer: "Noa Kirel", song: "Diamonds" },
+  { singer: "ACDC", song: "Thunderstruck" },
+  { singer: "AVoidIn", song: "The masses" },
 ];
