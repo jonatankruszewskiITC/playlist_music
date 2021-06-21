12d80695adc19633e71ed25519737d8ced02dc15
Jonatan <jonatan.kruszewski@itc.tech>
2021-06-21 13:28:12 +0300

    added Ricardo Monataner

--- playlist.js
+++ playlist.js
@@ -1,5 +1,6 @@
 const playlist = [
     { singer: "Maluma", song: "Despacito" },
     { singer: "Noa Kirel", song: "Diamonds" },
-    { singer: "ACDC", song: "Thunderstruck" }
+    { singer: "ACDC", song: "Thunderstruck" },
+    { singer: "Ricardo Monataner", song: "Song 1" }
 ];
